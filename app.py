92a9ca497bd5f9eb90859ccaa2cee1d5d5eabace
phat nguyen <batphat98@gmail.com>
2021-03-29 10:37:44 +0900

    メニューページの新規作成
    index.htmlからのメニューページへとして
    menu.htmlを新規作成しました

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def top():
     return render_template("index.html")
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
 if __name__=="__main__":
     app.run(debug=True)
